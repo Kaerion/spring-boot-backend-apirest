d5d4499302d7a34af9b0c58ca501177bbc7a3926
jquer <jquer@DESKTOP-JTCJECJ>
2022-10-28 13:06:26 +0200

    CORS implementation

--- src/main/java/com/nombreempresa/springboot/app/controllers/ClienteRestController.java
+++ src/main/java/com/nombreempresa/springboot/app/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.nombreempresa.springboot.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nombreempresa.springboot.app.models.entity.Cliente;
 import com.nombreempresa.springboot.app.models.service.IClienteService;
 
+@CrossOrigin(origins = { "http://localhost:8080" })
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
